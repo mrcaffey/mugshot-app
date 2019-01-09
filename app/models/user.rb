143f454e065de77f24b9bc432ba9ff084b82b09c
Micah Caffey <mrcaffey@gmail.com>
2019-01-09 15:09:36 -0700

    correction to syntax

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
-  has_many :posts, :comments
+  has_many :posts
+  has_many :comments
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
