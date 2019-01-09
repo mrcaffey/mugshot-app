47b903ea7043eeec43688f5cc9fd7796ab8f08a2
Micah Caffey <mrcaffey@gmail.com>
2019-01-09 15:37:39 -0700

    changes to gemfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  u = User.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    company_name: Faker::Lorem.word,
+    admin: Faker::Boolean.boolean,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number,
+    password: “password”
+  )
+end
