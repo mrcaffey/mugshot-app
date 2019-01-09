87ba31d669ff0744f784b8c0434aee6a53dae22c
Micah Caffey <mrcaffey@gmail.com>
2019-01-09 15:05:17 -0700

    associations for CRUD actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ActiveRecord::Base
+  has_many :posts, :comments
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
