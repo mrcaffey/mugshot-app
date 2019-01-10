1bcad9055995aa12b74d500d45551451a1e27b7b
Micah Caffey <mrcaffey@gmail.com>
2019-01-10 14:32:01 -0700

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,21 @@ require 'faker'
     email: Faker::Internet.email,
     password: "password"
   )
+
+
+20.times do 
+  Post.create(
+  author: u.id,
+  body: Faker::Movies::HitchhikersGuideToTheGalaxy.marvin_quote
+  )
+
+
+40.times do 
+  Comment.create(
+  author: u.id,
+  body: Faker::Movies::StarWars.quote
+  )
+
+end
+end
 end
