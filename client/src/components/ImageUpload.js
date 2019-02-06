dc687a3492375f7b9bd8844b8d2fb88d000baf85
Dirk Dobson <dirkvancouver@live.com>
2019-02-06 15:53:03 -0700

    upload progress

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class ImageUpload extends React.Component {
   state = {
@@ -10,9 +11,23 @@ class ImageUpload extends React.Component {
     })
   }
 
+  fileUploadHandler = () => {
+    const mugshot = new FormData();
+    mugshot.append('image', this.state.selectedFile, this.state.selectedFile.name)
+    axios.post("", mugshot, { onUploadProgress: progressEvent => {
+      console.log("upload Progress: " + Math.round(progressEvent.loaded / progressEvent.total * 100) + '%')
+    }})
+      .then(res => {
+        console.log(res)
+      });
+  }
+
   render() {
     return (
+      <>
       <input type="file" onChange={this.fileSelectedHandler} />
+      <button onClick={this.fileUploadHander}>Upload</button>
+      </>
     )
   }
 }
