77bf5bbad0343715df70c0c7abb8792a7b577a77
Dirk Dobson <dirkvancouver@live.com>
2019-02-06 16:05:08 -0700

    frontend image upload complete

--- client/src/components/ImageUpload.js
+++ client/src/components/ImageUpload.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 class ImageUpload extends React.Component {
   state = {
@@ -15,7 +16,7 @@ class ImageUpload extends React.Component {
     const mugshot = new FormData();
     mugshot.append('image', this.state.selectedFile, this.state.selectedFile.name)
     axios.post("", mugshot, { onUploadProgress: progressEvent => {
-      console.log("upload Progress: " + Math.round(progressEvent.loaded / progressEvent.total * 100) + '%')
+      document.getElementById("progress").innerHTML = 'upload Progress: ' + Math.round(progressEvent.loaded / progressEvent.total * 100) + '%'
     }})
       .then(res => {
         console.log(res)
@@ -24,10 +25,18 @@ class ImageUpload extends React.Component {
 
   render() {
     return (
-      <>
+      <Body>
       <input type="file" onChange={this.fileSelectedHandler} />
       <button onClick={this.fileUploadHander}>Upload</button>
-      </>
+      <p id="progress"></p>
+      </Body>
     )
   }
 }
+
+const Body = styled.div`
+padding-top: 10em;
+`
+
+
+export default ImageUpload
