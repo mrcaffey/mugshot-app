4dfc0dd6f928093030acf41af351d272f34b3f49
Dirk Dobson <dirkvancouver@live.com>
2019-02-11 17:22:36 -0700

    permit parameters in users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -36,6 +36,6 @@ class Api::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit_all_parameters
+    params.require(:user).permit(:avatar, :name, :email, :nickname)
   end
 end
