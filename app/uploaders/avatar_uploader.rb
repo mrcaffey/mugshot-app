01392192ba755f4f71ea78d8e069bd4df5c259e8
Dirk Dobson <dirkvancouver@live.com>
2019-02-12 19:02:01 -0700

    new folder to store avatars

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -10,7 +10,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "client/Images"
+    "client/Images/Avatars"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
